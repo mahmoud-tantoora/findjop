e657a628e571441aea27d1bde7aabd3b8ecf94d3
Your Name <you@example.com>
2019-06-22 06:43:48 +0200

    first commit

---
+++ app/src/main/java/com/example/dell/navbot/Itemdata.java
@@ -0,0 +1,14 @@
+package com.example.dell.navbot;
+
+public class Itemdata {
+    public  String title;
+    public String detail;
+    public int image;
+    public Itemdata(String title,String detail,int image)
+    {
+        this.title=title;
+        this.detail=detail;
+        this.image=image;
+    }
+
+}
